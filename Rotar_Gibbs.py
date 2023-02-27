726bd8892e4a35496e64cf0f9d6b83e85fb4164b
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-02-27 17:17:53 -0600

    Commented function

--- Rotar_Gibbs.py
+++ Rotar_Gibbs.py
@@ -4,6 +4,18 @@ from math import sin, cos, pi
 
 
 def rotate(u: np.ndarray, ang: float, eje: np.ndarray) -> np.ndarray:
+    """
+    Rotate the vector 'u' around the axis 'eje' 'ang' degrees.
+
+    Parameters:
+    u (np.ndarray): Vector to be rotated.
+    ang (float): Angle, in degrees, used to rotate the vector 'u'.
+    eje (np.ndarray): Axis around which to rotate the vector 'u'.
+
+    Returns:
+    np.ndarray: Rotated vector 'u' around the axis 'eje' with a rotation of 'ang' degrees.
+    """
+
     ang = ang * (pi / 180)
     uni = eje / norm(eje)
     Se = np.array(
