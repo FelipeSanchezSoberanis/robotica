867caa667850ec0d603c783de1f83abe0d257ce0
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-03-09 17:22:17 -0600

    Symbolic rotation matrix

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-from rotar_gibbs import rotate
-import numpy as np
 import sympy as sp
+
+
+def rotate(e: sp.Matrix, theta: sp.Symbol) -> sp.Matrix:
+    ex: sp.Symbol = e[0, 0]  # type: ignore
+    ey: sp.Symbol = e[1, 0]  # type: ignore
+    ez: sp.Symbol = e[2, 0]  # type: ignore
+
+    se = sp.Matrix([[0, -ez, ey], [ez, 0, -ex], [-ey, ex, 0]])
+
+    return e @ e.T + (sp.eye(3) - e @ e.T) * sp.cos(theta) + se * sp.sin(theta)
 
 
 def main():
     ex, ey, ez = sp.symbols("ex ey ez")
-    theta = 30
-    u = np.array([1, 1, 1])
-    eje = np.array([ex, ey, ez])
+    theta = sp.symbols("theta")
 
-    W = rotate(u, theta, eje)
+    e = sp.Matrix([[ex], [ey], [ez]])
 
-    print(W)
+    r = rotate(e, theta)
+    sp.pprint(r)
 
 
 if __name__ == "__main__":
