f8526ea3939cd15dd3821c22a7c33fbd90f40448
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-03-11 14:12:15 -0600

    WIP

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ import numpy as np
 import sympy as sp
 
 
-def main():
-    ex, ey, ez = sp.symbols("ex ey ez")
-    theta = sp.symbols("theta")
+def translate(x: sp.Symbol, y: sp.Symbol, z: sp.Symbol):
+    part1 = np.append(np.eye(3), sp.Matrix([[x], [y], [z]]), axis=1)
+    result = np.append(part1, np.array([[0, 0, 0, 1]]), axis=0)
+    print(result)
 
-    print(num_op.rotate(np.array([[15], [2], [2]]), 30))
-    sp.pprint(sym_op.rotate(sp.Matrix([[ex], [ey], [ez]]), theta))
+
+def main():
+    #  T = np.array([[0.86, -0.5, 0, 10], [0.5, 0.86, 0, 5], [0, 0, 1, 0], [0, 0, 0, 1]])
+    #  P1 = np.array([[3], [7], [0], [1]])
+    #  P2 = T.dot(P1)
+    x, y, z = sp.symbols("x y z")
+    translate(x, y, z)
 
 
 if __name__ == "__main__":
