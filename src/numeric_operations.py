e477b3e09270f77a8f390f9aef1c0e52a3671c06
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-03-31 14:01:16 -0600

    Docstrings

--- src/numeric_operations.py
+++ src/numeric_operations.py
@@ -2,17 +2,45 @@ import numpy as np
 
 
 def mat_rot_gibbs(e: np.ndarray, phi: float) -> np.ndarray:
+    """
+    Calculates the Gibbs rotation matrix when rotating "phi" degrees around the
+    axis "e".
+
+    Parameters:
+    - e (np.ndarray): Axis of rotation. Can be of shape (1, 3) or (3, 1).
+    - phi (float): Degrees of rotation. Has to be in degrees, not radians.
+
+    Returns:
+    - np.ndarray: Gibbs rotation matrix. Has shape (3, 3).
+    """
     e = e.reshape(3, 1)
     phi = np.deg2rad(phi)
 
     ex, ey, ez = e[0, 0], e[1, 0], e[2, 0]
     se = np.array([[0, -ez, ey], [ez, 0, -ex], [-ey, ex, 0]], dtype=float)
-    return e @ e.T + np.cos(phi) * (np.eye(3) - e @ e.T) + np.sin(phi) * se
+    mat_rot = e @ e.T + np.cos(phi) * (np.eye(3) - e @ e.T) + np.sin(phi) * se
+
+    return mat_rot
 
 
 def mat_trans_homo(
     e: np.ndarray, phi: float, vt: np.ndarray, sf: float, vp: np.ndarray
 ) -> np.ndarray:
+    """
+    Calculates the homogenous transformation matrix given an axis of rotation,
+    degree of rotation, vector of translation, scaling factor and vector of
+    perspective.
+
+    Parameters:
+    - e (np.ndarray): Axis of rotation. Can be of shape (1, 3) or (3, 1).
+    - phi (float): Degrees of rotation. Has to be in degrees, not radians.
+    - vt (np.ndarray): Translation vector. Can be of shape (1, 3) or (3, 1).
+    - sf (float): Scaling factor.
+    - vp (np.ndarray): Perspective vector. Can be of shape (1, 3) or (3, 1).
+
+    Returns:
+    - np.ndarray: Homogenous transformation matrix. Has shape (4, 4).
+    """
     e = e.reshape(3, 1)
     vt = vt.reshape(3, 1)
     vp = vp.reshape(1, 3)
