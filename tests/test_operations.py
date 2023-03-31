a21e3a49f3571d65b6a7bed05260c772c22081a5
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-03-31 14:29:42 -0600

    Test transform and trans inv relation

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 import src.numeric_operations as num_op
+import random
 
 
 class TestOperations(unittest.TestCase):
@@ -45,3 +46,21 @@ class TestOperations(unittest.TestCase):
         )
 
         np.testing.assert_almost_equal(calculated_mat_homo_inv, expected_mat_homo_inv)
+
+    def test_transformation_chaining(self):
+        e = np.array([[0], [0], [1]])
+        phi = 30
+        vt = np.array([[4], [3], [0]])
+        sf = 1
+        vp = np.zeros((1, 3))
+
+        rand_int = lambda: random.randint(-1_000_000, 1_000_000)
+
+        p_original = np.array([[rand_int()], [rand_int()], [rand_int()], [1]])
+
+        mat_trans = num_op.mat_trans_homo(e, phi, vt, sf, vp)
+        mat_trans_inv = num_op.mat_trans_homo_inv(e, phi, vt, sf, vp)
+
+        p_translated = mat_trans @ p_original
+
+        np.testing.assert_almost_equal(mat_trans_inv @ p_translated, p_original)
